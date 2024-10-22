10f23a8944c0ad3562981f40dcb74644ea4d5afc
bushra786-dot <bushrakhalid850@gmail.com>
2024-10-22 15:21:06 +0500

    Add files via upload

---
+++ Lab11.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab11 {
+
+
+	public static void main(String[] args) {
+		List <String> cars = new ArrayList<String>();
+		 cars.add("Volvo");
+		 cars.add("BMW");
+		 cars.add("Ford");
+		 cars.add("Mazda");
+		 System.out.println(cars);
+		 } 
+		
+
+	}
+		
+		
+	
+
+
